c004b620b3b5dde049b8f9926993e09fceb56c9a
David Gillies <dgillies@brandscreen.com>
2013-04-03 10:44:41 +1100

    added type parameter for setting types to keys eg: string, bool, int, array, etc added option for defaults host option

--- manifests/osx_defaults.pp
+++ manifests/osx_defaults.pp
@@ -3,20 +3,35 @@ define osx_defaults(
   $domain = undef,
   $key    = undef,
   $value  = undef,
-  $user   = undef,
+  $host   = undef,
+  $user   = 'root',
+  $type   = 'string',
 ) {
-  $defaults_cmd = "/usr/bin/defaults"
+  $defaults_cmd = '/usr/bin/defaults'
+
+  if $host == undef {
+    $host_arg = '-currentHost'
+  } else {
+    $host_arg = "-host ${host}"
+  }
 
   if $ensure == 'present' {
-    if ($domain != undef) and ($key != undef) and ($value != undef) {
+    if $type =~ /string|data|int|float|bool|date|array|array-add|dict|dict-add/ {
+      $type_arg = "-${type}"
+    } else {
+      warn('Invalid type declared')
+    }
+
+    if $domain or $key or $value != undef {
       exec { "osx_defaults write ${domain}:${key}=>${value}":
-        command => "${defaults_cmd} write ${domain} ${key} ${value}",
+        command => "${defaults_cmd} ${host_arg} write ${domain} ${type_arg} ${key} ${value}",
         unless  => "${defaults_cmd} read ${domain} ${key}|egrep '^${value}$'",
         user    => $user
       }
     } else {
-      warn("Cannot ensure present without domain, key, and value attributes")
+      warn('Cannot ensure present without domain, key, and value attributes')
     }
+
   } else {
     exec { "osx_defaults delete ${domain}:${key}":
       command => "${defaults_cmd} delete ${domain} ${key}",
